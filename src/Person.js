a690ee4476b7623486385a5ad3b401e766ad4139
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-11-26 20:52:50 +0530

    react element used

---
+++ src/Person.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Person extends Component{
+    render(){
+        // return(
+        // <div className="App-header">
+        //     <i>
+        //         <h1>Hello GFG</h1>
+        //     </i>
+        // </div>
+        // );
+
+ 
+    }
+}
+export default Person;
